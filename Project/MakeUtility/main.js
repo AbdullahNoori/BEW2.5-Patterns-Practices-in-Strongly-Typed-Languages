7b558b9a11360d9c4515c746e1c0755f8bc6102c
A.Noori <nooriabdullah86@gmail.com>
2021-03-01 00:32:14 -0800

    Add files via upload

---
+++ Project/MakeUtility/main.js
@@ -0,0 +1,6 @@
+// implement the websockets client functionality for the frontend
+// removes front elements from From Javascript DOM using the document.querySelector() function
+const input = document.querySelector('#textarea')
+const messages = document.querySelector('#messages')
+const username = document.querySelector('#username')
+const send = document.querySelector('#send')
