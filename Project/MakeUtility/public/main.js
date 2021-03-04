83b26882099ff6e54c8b8df86657ea0edcde6e00
A.Noori <nooriabdullah86@gmail.com>
2021-03-04 01:29:40 -0800

    Add files via upload

--- Project/MakeUtility/public/main.js
+++ Project/MakeUtility/public/main.js
@@ -31,9 +31,10 @@ function insertMessage(messageObj) {
 	const message = document.createElement('div')
 
 	// Sets the attribute of the message 
-	message.setAttribute('class', 'chat')
+	message.setAttribute('class', 'chat text-white', 'message')
 	console.log("name: " +messageObj.username + " content: " + messageObj.content)
 	message.textContent = `${messageObj.username}: ${messageObj.content}`
+
 
 	// Appends the message to my chat div
 	messages.appendChild(message)
@@ -41,6 +42,26 @@ function insertMessage(messageObj) {
 	// Inserts the message as the first message of our chat
 	messages.insertBefore(message, messages.firstChild)
 }
+
+
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	d := time.Date(2000, 2, 1, 12, 30, 0, 0, time.UTC)
+// 	day := d.Day()
+
+// 	fmt.Printf("day = %v\n", day)
+
+// }
+
+
+
 
 
 
